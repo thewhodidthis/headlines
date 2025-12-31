d32627013925655008c03c05860fc6ab5319b73f
thewhodidthis <me@thewhodidthis.com>
2025-12-31 11:04:56 +0000

    iife: rebuild

--- headlines.js
+++ headlines.js
@@ -227,17 +227,17 @@ var headlines = (function() {
             </p>`)
           )
 
-        if (!items.length) {
-          throw new Error("Nothing to show")
+        if (items.length) {
+          // Update container.
+          wrap.append(...items)
+
+          const completeEvent = new CustomEvent("complete", { detail: items.length })
+
+          // Signal success.
+          host.dispatchEvent(completeEvent)
         }
 
-        // Update container.
-        wrap.append(...items)
-
-        const completeEvent = new CustomEvent("complete", { detail: items.length })
-
-        // Signal success.
-        host.dispatchEvent(completeEvent)
+        throw new Error("Nothing to show")
       } catch ({ message }) {
         const item = range.createContextualFragment(`
         <p>
